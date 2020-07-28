4b4d155a35b7451257845319b4907bdffa1b19b5
Kevin Schechter <keschech@microsoft.com>
2020-07-28 18:53:05 -0400

    elev8 methods

--- elev8/note.js
+++ elev8/note.js
@@ -1,9 +1,13 @@
+const jsMidiId = [[]];
+
 class Note {
 	constructor(neckString, fret) {
 		this.neckString = neckString;
 		this.fret = fret;
-		this.x = neckString;
-		this.y = fret;
 		this.jsMidiId = 40;
-	}
+    }
 };
+
+//  DRAWGUITAR.drawCircle(0, 5);
+// 	DRAWGUITAR.drawCircle(1, 5);
+// 	DRAWGUITAR.drawCircle(2, 5);
