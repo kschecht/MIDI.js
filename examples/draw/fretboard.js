a7221f70cf9cbd5425b4142a29625c5a247c808c
Mike Francis <mike.francis@live.com>
2020-07-29 09:17:57 -0700

    added midi notes - needs fixing

--- examples/draw/fretboard.js
+++ examples/draw/fretboard.js
@@ -1,3 +1,8 @@
+// import "../js/midi/audioDetect.js";
+// import "../js/midi/loader.js" 
+// import "../js/midi/plugin.audiotag.js" 
+// import "../js/midi/plugin.webaudio.js"
+// import "../js/midi/plugin.webmidi.js"
 
 if (typeof DRAWGUITAR === 'undefined') DRAWGUITAR = {};
 
@@ -54,7 +59,7 @@ root.drawFretboard = function () {
     ctx.stroke();
     // horizontal lines
     ctx.beginPath();
-    ctx.moveTo(10 * scale, 30 * scale);
+    ctx.moveTo(xStart * scale, yStart * scale);
     ctx.lineTo(160 * scale, 30 * scale);
     ctx.stroke();
 
@@ -96,7 +101,7 @@ root.drawFretboard = function () {
 
     };
 
-
+    
     // frets 0 - 17
     // C chord == [0][4], [1,2], [2,2]
     // Em chord == [1,1], [1,2]
@@ -111,10 +116,42 @@ root.drawFretboard = function () {
         // 12, 13, 14, 15, 16, 17
         [false, false, false, false, false, false]
       ];
+    //   String	Fret	Midi ID	Midi ID (left)
+    //   0	0	40	64
+    //   0	1	41	65
+    //   0	2	42	66
+    //   1	0	45	59
+    //   1	1	46	60
+    //   1	2	47	61
+    //   2	0	50	55
+    //   2	1	51	56
+    //   2	2	52	57
+    //   3	0	55	50
+    //   3	1	56	51
+    //   3	2	57	52
+    //   4	1	59	45
+    //   4	2	60	46
+    //   4	2	61	47
+    //   5	0	64	40
+    //   5	1	65	41
+    //   5	2	66	42
+      
+      var midiNotes = [
+          [[40,64], [45,59], [60,55], [55,50], [59,45], [64,68]],          
+          [[41,65], [46,60], [51,56], [56,51], [60,46], [65,41]],          
+          [[42,66], [47,61], [52,57], [57,52], [61,47], [68,42]],          
+      ];
 
+      var notesToPlay = [];
+
+function updateNotesToPlay(row, col) {
+    var note = midiNotes[row][col][1]
+    notesToPlay.push(note);
+}
 
 root.showChord=function(chordName)
 {
+    
     // Clear fretBoard
      frets[0].fill(false);
      frets[1].fill(false);
@@ -136,16 +173,21 @@ root.showChord=function(chordName)
         break;
         case 'Em':
         // Em Chord
-        frets[1][1]=true;
-        frets[1][2]=true;
+        frets[1][1]=true; // midi id 41
+        frets[1][2]=true; // midi id 47
         correctChordFlag=true;
         break;
         case 'C':
         // C Chord
-        frets[0][4]=true;
-        frets[1][2]=true;
+        frets[0][4]=true; 
+        frets[1][2]=true; 
         frets[2][1]=true;
         correctChordFlag=true;
+        playSound(48);
+        playSound(52);
+        playSound(55);
+        playSound(60);
+        playSound(64);
         break;
         case 'G':
         // G Chord
@@ -154,8 +196,8 @@ root.showChord=function(chordName)
         frets[2][5]=true;
         correctChordFlag=true;
         break;
-        case 'X':
-        // X Chord - wrong chord
+        case '?':
+        // ? Chord - wrong chord
         frets[1][0]=true;
         frets[2][4]=true;
         frets[2][5]=true;
@@ -163,8 +205,40 @@ root.showChord=function(chordName)
 
     }
     drawChord(frets, correctChordFlag);
+    updateChordIndicator(chordName)
+
+}
 
 
+function playSound(note1) {
+    MIDI.loadPlugin({
+    soundfontUrl: "./soundfont_guitar/",
+    instrument: "acoustic_guitar_steel",
+    onprogress: function(state, progress) {
+        console.log(state, progress);
+    },
+    onsuccess: function() {
+        var delay = 0; // play one note every quarter second
+        // var note1 = 41; // the MIDI note
+        var velocity = 127; // how hard the note hits
+        // play the note
+        MIDI.programChange(0, MIDI.GM.byName["acoustic_guitar_steel"].number); // select correct instrument
+        MIDI.setVolume(0, 300);
+        MIDI.noteOn(0, note1, velocity, delay);
+        MIDI.noteOff(0, note1, delay + 0.75);
+
+        // var note2 = 52; // the MIDI note
+        // MIDI.noteOn(0, note2, velocity, delay);
+        // MIDI.noteOff(0, note2, delay + 0.75);
+
+    }
+});
+}
+
+function updateChordIndicator(chordName)
+{
+    var cn = document.getElementById("chordName");
+    cn.innerText=chordName;
 
 }
 
@@ -177,6 +251,7 @@ for (row=0;row<3;row++)
     if (frets[row][col] == true)
     {
         root.drawCircle(row, col, correctChordFlag);
+        updateNotesToPlay(row,col)
     }
 
     for (col=0;col<6;col++)
@@ -184,6 +259,7 @@ for (row=0;row<3;row++)
         if (frets[row][col] == true)
         {
             root.drawCircle(row, col, correctChordFlag);
+            updateNotesToPlay(row,col)
         }
     }
 }
