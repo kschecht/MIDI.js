027ea4681bda534c9a97abf30d5269fa4322eb51
Sarah Brown <sarbrown@microsoft.com>
2020-07-29 12:13:23 -0700

    handling servers

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end(dataInput);
+// }).listen(8080);
+
+//listen to local host
+var express = require('express');
+
+   var app = express();
+   var http = require('http').Server(app);
+   var server = http.listen(4000, "0.0.0.0", () => { //Start the server, listening on port 4000.
+       console.log("Listening to requests on port 4000...");
+   })
+   app.use(express.static('public')); //Send index.html page on GET /
+
+   var io = require('socket.io')(server); //Bind socket.io to our express server.
+   //var io = socket(server);
+
+
+//get serial port data
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('COM4', { baudRate: 9600 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-var dataInput;
+const serialPort = new SerialPort('COM4', { baudRate: 9600 });
+const parser = serialPort.pipe(new Readline({ delimiter: '\n' }));
 // Read the port data
-port.on("open", () => {
+serialPort.on("open", () => {
   console.log('serial port open');
 });
 parser.on('data', data =>{
-  dataInput = data;
-  console.log(data);
+ //console.log(data);
+    io.emit('emit_serial_data', data);
+    console.log("post emit");
 });
+//
+//
+//
+io.on('connection', (socket) => {
+      console.log("Someone connected."); //show a log as a new client connects.
+  });
+//
